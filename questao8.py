e7ebcc4e12360613b7a67fdcf92bcf319ad15140
Gustavo Baptista <chustaffus777@gmail.com>
2023-08-08 11:24:19 -0300

    Tarefa Concluída!

--- questao8.py
+++ questao8.py
@@ -1,4 +1,9 @@
 #8) Fazer um algoritmo que receba o preço de custo de um produto e mostre como resposta o valor de venda. Sabe se que o preço de custo receberá um acréscimo de acordo com um percentual informado pelo usuário.
 
 custo = float(input("Qual é o valor do produto?"))
+per = float(input("Qual é o percentual de Acréscimo?"))
 
+acr = custo * per/100
+final = custo + acr
+
+print(f"O Valor total da venda será de R${final}0.")
